a4b87f586838447c6959be28c50eac73c712927f
Frederick Thomas <frederickdthomasjr@gmail.com>
2019-01-20 19:38:05 -0800

    Add files via upload

---
+++ docs/script.js
@@ -0,0 +1,5 @@
+// This awesome code can be found at https://gist.github.com/CodeMyUI/97d2ea07e2299c891a68d74f5073e34f ; and the tutorial is at https://codemyui.com/fade-intro-text-animation/  Please feel free to Fork this code; and try your own combinations.//
+$("#os-phrases > h2")
+    .css('opacity', 1).lettering( 'words' )
+    .children( "span" ).lettering()
+    .children( "span" ).lettering(); 
